9a4b0d859b29c52c65552657685094e3d9cf7696
prnv-dxt <dev.pranavdixit@gmail.com>
2021-07-18 15:34:12 +0530

    Second Commit

--- FaceMeshModule.py
+++ FaceMeshModule.py
@@ -2,55 +2,58 @@ import cv2
 import mediapipe as mp
 import time
 
+
 class FaceMeshDetector():
-    def __init__(self, staticMode=False, maxFace=2, minDetectionCon=0.5, minTrackCon=0.5):
+    def __init__(self, staticMode=False, maxFaces=2, minDetectionCon=0.5, minTrackCon=0.5):
         self.staticMode = staticMode
-        self.maxFace = maxFace
+        self.maxFaces = maxFaces
         self.minDetectionCon = minDetectionCon
-        self.minTrackingCon = minTrackCon
+        self.minTrackCon = minTrackCon
 
         self.mpDraw = mp.solutions.drawing_utils
         self.mpFaceMesh = mp.solutions.face_mesh
-        self.faceMesh = self.mpFaceMesh.FaceMesh(self.staticMode, self.maxFace, self.minDetectionCon, self.minTrackingCon)
+        self.faceMesh = self.mpFaceMesh.FaceMesh(self.staticMode, self.maxFaces, self.minDetectionCon, self.minTrackCon)
         self.drawSpecs = self.mpDraw.DrawingSpec(thickness=1, circle_radius=1)
 
     def findFaceMesh(self, img, draw=True):
         self.imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.faceMesh.process(self.imgRGB)
         faces = []
-
         if self.results.multi_face_landmarks:
+
             for faceLms in self.results.multi_face_landmarks:
                 if draw:
                     self.mpDraw.draw_landmarks(img, faceLms, self.mpFaceMesh.FACE_CONNECTIONS, self.drawSpecs, self.drawSpecs)
-
                 face = []
                 for id, lm in enumerate(faceLms.landmark):
-                    # print(lm)
+                    print(lm)
                     ih, iw, ic = img.shape
                     x, y = int(lm.x * iw), int(lm.y * ih)
-                    # cv2.putText(img, str(id), (x, y), cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 255, 0), 1)
+                    # cv2.putText(img, str(id), (x, y), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
 
                     # print(id, x, y)
-                    face.append([x,y])
+                    face.append([x, y])
                 faces.append(face)
         return img, faces
 
+
 def main():
-    cap = cv2.VideoCapture(0)
+
+    cap = cv2.VideoCapture("Videos/2.mp4")
     pTime = 0
     detector = FaceMeshDetector()
-
     while True:
         success, img = cap.read()
         img, faces = detector.findFaceMesh(img)
-
         if len(faces) != 0:
-            print(faces[0])
+            print(faces)
 
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
+        try:
+            cTime = time.time()
+            fps = 1/(cTime - pTime)
+            pTime = cTime
+        except ZeroDivisionError:
+            pass
 
         cv2.putText(img, f'FPS {int(fps)}', (20, 40), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0), 1)
         cv2.imshow("Image", img)
